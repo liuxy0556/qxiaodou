8c3b58e1c7cfd485df43fbae7573526be235277c
liuyi <liuxy0551@qq.com>
2019-06-24 10:46:05 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+  // title: 'Q小荳后台管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
